85ac9845780a4cbbc75b00150d8308d00b79cc2d
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2022-12-03 13:38:01 -0800

    implemen the function of searching card by inputting string

--- api_ygo/searchByName.php
+++ api_ygo/searchByName.php
@@ -4,27 +4,68 @@
     $dataString = fread($dataFile, filesize("./data/ygo.txt"));
     $decodedData = json_decode($dataString, true);
     fclose($dataFile);
+
+    $keyCount = 0;
     foreach($decodedData as $key => $cards) {
+        //get first element of json data. this data has the key ["data"]. it has only one hash on the top level. under ["data"], there is the data of each card.
         foreach($cards as $key => $value) {
+            if($keyCount == 0) $num = array_keys($cards);
+            //get information of each card here.
             if(is_array($value)) {
-                foreach($value as $key2 => $value2) {
-                    if(is_array($value2)) {
-                        foreach($value2 as $key3 => $value3) {
-                            if(is_array($value3)) {
-                                foreach($value3 as $key4 => $value4) {
-                                    echo "<p>$key4 : $value4</p>";
+                if(strpos($value["name"], $name) !== false) {
+                    foreach($value as $key2 => $value2) {
+                        if(is_array($value2)) {
+                            foreach($value2 as $key3 => $value3) {
+                                if(is_array($value3)) {
+                                    foreach($value3 as $key4 => $value4) {
+                                        echo "<p>$key4 : $value4 : $num[$keyCount]</p>";
+                                    }
+                                } else {
+                                    echo "<p>$key3 : $value3 : $num[$keyCount]</p>";
                                 }
-                            } else {
-                                echo "<p>$key3 : $value3</p>";
                             }
+                        } else {
+                            echo "<p>$key2 : $value2 : $num[$keyCount]</p>";
                         }
-                    } else {
-                        echo "<p>$key2 : $value2</p>";
+                        // echo "<h1>not found</h1>";
                     }
+                    $keyCount++;
+                } else {
+                    // echo "<p>".$value["name"] . " " . "failed"."</p>";
                 }
-            } else {
-                echo "<p>$key : $value</p>";
-            }
+                } else {
+                    echo "<h1>not found!</h1>";
+                }
+            // $keyCount++;
         }
     }
+    // foreach($decodedData as $key => $cards) {
+    //     //get first element of json data. this data has the key ["data"]. it has only one hash on the top level. under ["data"], there is the data of each card.
+    //     foreach($cards as $key => $value) {
+    //         if($keyCount == 0) $num = array_keys($cards);
+    //         //get information of each card here.
+
+    //         if(is_array($value)) {
+    //             foreach($value as $key2 => $value2) {
+    //                 if(is_array($value2)) {
+    //                     foreach($value2 as $key3 => $value3) {
+    //                         if(is_array($value3)) {
+    //                             foreach($value3 as $key4 => $value4) {
+    //                                 echo "<p>$key4 : $value4 : $num[$keyCount]</p>";
+    //                             }
+    //                         } else {
+    //                             echo "<p>$key3 : $value3 : $num[$keyCount]</p>";
+    //                         }
+    //                     }
+    //                 } else {
+    //                     echo "<p>$key2 : $value2 : $num[$keyCount]</p>";
+    //                     echo "<p>".$value["name"]."</p>";
+    //                 }
+    //             }
+    //         } else {
+    //             echo "<h1>get card!</h1>";
+    //         }
+    //         $keyCount++;
+    //     }
+    // }
 ?>
