142f9d7253d696d1357c2b6d857ad733bdb40916
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2023-01-01 23:28:52 -0800

    added what i did in the class

---
+++ reactphp/src/Php.tsx
@@ -0,0 +1,7 @@
+import react from "react";
+
+function Php() {
+    return (
+        console.log("done")
+    )
+}
---
+++ reactphp/src/first.php
@@ -0,0 +1,23 @@
+<?php
+    echo "<p>hello world</p>";
+    echo "<p>hello world</p>";
+    echo "<p>hello world</p>";
+    echo "<p>hello world</p>";
+    echo "<p>hello world</p>";
+    echo "<p>hello world</p>";
+    echo "<p>hello world</p>";
+    echo "<p>hello world</p>";
+    echo "<p>hello world</p>";
+    echo "<p>hello world</p>";
+    echo "<p>hello world</p>";
+    echo "<p>hello world</p>";
+    echo "<p>hello world</p>";
+    echo "<p>hello world</p>";
+    echo "<p>hello world</p>";
+    echo "<p>hello world</p>";
+    echo "<p>hello world</p>";
+    echo "<p>hello world</p>";
+    echo "<p>hello world</p>";
+    echo "<p>hello world</p>";
+    echo "<p>hello world</p>";
+?>
